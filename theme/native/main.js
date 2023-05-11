eb8a3ee9c13449a5888d1247d96799a2df8d215e
hedwig.doets <hedwig.doets@mendix.com>
2023-05-11 13:23:09 +0200

    Add version 3.0.0 of AR StarterApp

---
+++ theme/native/main.js
@@ -0,0 +1,51 @@
+export {};
+
+export const introImage = {
+    image: {
+      aspectRatio: 1,
+      width: "75%",
+      height: null,
+    },
+  };
+  
+  export const introImageBusinessCard = {
+    image: {
+      aspectRatio: 1.775,
+      width: "50%",
+      height: null,
+    },
+  };
+  
+  export const absoluteBottom = {
+    position: "absolute",
+    bottom: 0,
+  };
+  
+  export const homeIcon = {
+    container: {
+      flex: 2,
+    },
+    image:{
+      resizeMode: 'contain'
+    }
+  }
+  
+  export const flex2 = {
+    container: {
+      flex: 2
+    }
+  }
+
+  export const sliderOrange = {
+    highlight: {
+        height: 4,
+        backgroundColor: "#FCA538"
+    }
+  };
+
+  export const sliderBlue = {
+    highlight: {
+      height: 4,
+      backgroundColor: "#0CABF9"
+  }
+}
